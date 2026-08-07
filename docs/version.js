346b4224612d62c0c57f478867cfd1244838a07d
miguelsgarcia4 <mgbouldering@gmail.com>
2026-08-07 19:02:17 -0400

    Update About panel build info to latest commit

--- docs/version.js
+++ docs/version.js
@@ -9,7 +9,7 @@
  * commit. Stays below 1.0 until the app is actually considered a 1.0.
  */
 export const BUILD_INFO = {
-  version: '0.7',
-  commit: 'b47c5f5',
-  builtAt: '2026-08-07T22:47:00-04:00',
+  version: '0.8',
+  commit: '5969d37',
+  builtAt: '2026-08-07T22:52:00-04:00',
 };
